1d116935fdc889d99aa5db3103da7227b495e306
Lei Qian <lei.qian@amlogic.com>
2018-12-10 03:27:53 -0800

    Revert "LiveTv: add switch channel period to 500ms [1/1]"
    This reverts commit c26dba83ad729a2a49804a88dad6300c4f8f5d52.
    
    Change-Id: I6b5d39094e7c8bbfe20022c3d6bf588cedde295a

--- src/com/android/tv/MainActivity.java
+++ src/com/android/tv/MainActivity.java
@@ -259,7 +259,6 @@ public class MainActivity extends Activity implements OnActionClickListener, OnP
     private static final int MSG_CHANNEL_DOWN_PRESSED = 1000;
     private static final int MSG_CHANNEL_UP_PRESSED = 1001;
     private static final int MSG_FILTER_CEC_OTP_TIMEOUT = 1002;
-    private static final int MSG_TUNE_CHANNEL = 1003;
 
     private static final int TVVIEW_SET_MAIN_TIMEOUT_MS = 3000;
     private static final int DELAY_TIMEOUT_MS = 10000;
@@ -3115,15 +3114,13 @@ public class MainActivity extends Activity implements OnActionClickListener, OnP
         }
         mHandler.removeMessages(MSG_CHANNEL_UP_PRESSED);
         mHandler.removeMessages(MSG_CHANNEL_DOWN_PRESSED);
-        mHandler.removeMessages(MSG_TUNE_CHANNEL);
         if (mChannelTuner.getBrowsableChannelCount() > 0) {
             if (!mTvView.isPlaying()) {
                 // We expect that mTvView is already played. But, it is sometimes not.
                 // TODO: we figure out the reason when mTvView is not played.
                 Log.w(TAG, "TV view isn't played in finishChannelChangeIfNeeded");
             }
-            //tuneToChannel(mChannelTuner.getCurrentChannel());
-            mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_TUNE_CHANNEL, mChannelTuner.getCurrentChannel()), CHANNEL_CHANGE_INITIAL_DELAY_MILLIS);
+            tuneToChannel(mChannelTuner.getCurrentChannel());
         } else {
             if (!USE_DROIDLOIC_CUSTOMIZATION) {
                 showSettingsFragment();
@@ -3548,10 +3545,6 @@ public class MainActivity extends Activity implements OnActionClickListener, OnP
                     mFilterOtpEnabled = false;
                     if (DEBUG) Log.d(TAG, "reset mFilterOtpEnabled to false");
                     break;
-                case MSG_TUNE_CHANNEL:
-                    Channel channel = (Channel) msg.obj;
-                    mainActivity.tuneToChannel(channel);
-                    if (DEBUG) Log.d(TAG, "MSG_CHANNEL_TUNE");
                 default: // fall out
             }
         }
